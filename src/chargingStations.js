4a9eb11df388ebdb9b3311c16f09ed87f720a8ee
Lowella Fox <foxlo25@c02dqecjmd6m.uswin.ad.vzwcorp.com>
2021-11-27 00:49:13 -0600

    Finalizing codes

--- src/chargingStations.js
+++ src/chargingStations.js
@@ -1,18 +1,13 @@
-const Scooter = require('./scooter')
-
 class ChargingStations {
     constructor(){
-        this.stations = []
+        this.station = []
         this.scooterIDs = []
     }
 
-    addScooter(location,scooterID){
-        this.stations = location
+    addScooter(location, scooterID){
+        this.station = location
         this.scooterIDs.push(scooterID)
     }
-
-    removedScooter(location, scooter)
 }
 
-const stations = new ChargingStations()
-console.log(stations)
+module.exports = ChargingStations
--- src/scooter.js
+++ src/scooter.js
@@ -1,12 +1,43 @@
+const User = require('../src/user')
+
 class Scooter {
-    constructor(user,scooterID, station,isBroken,isCharged,serialNumber){
+    constructor(user,scooterID,station,isBroken,isFullyCharged,serialNumber){
         this.user = user
         this.scooterID = scooterID
         this.station = station
         this.isBroken = isBroken
-        this.isCharged = isCharged
+        this.isFullyCharged = isFullyCharged
+        this.isAvailable = true
         this.serialNumber = serialNumber
     }
+
+    async charge() {
+        console.log('Starting charge');
+
+        await new Promise(resolve => setTimeout(resolve, 2000)); // wait 2 seconds
+
+        this.isFullyCharged = true
+        this.isBroken = false
+        this.isAvailable = true
+        console.log('Charge complete');   
+   }
+
+    rent(){
+        if(this.isBroken || !this.isFullyCharged) {
+            this.isAvailable = false
+            console.log('Scooter is broken or low on battery.')
+        }
+        else {
+            console.log('Thanks for renting! Enjoy your ride!')
+        }
+    }
+
+    repair(){
+        if(!this.isBroken && this.isFullyCharged && this.isAvailable){
+            console.log('Repair is complete')
+        }
+    }
+    
 }
 
 module.exports  = Scooter
